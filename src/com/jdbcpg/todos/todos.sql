fefc655eb2db41846f6ca5d04ee4c209ce91eb56
clydeatuic <clydeatuic@gmail.com>
2017-09-11 13:43:26 +0800

    first commit

---
+++ src/com/jdbcpg/todos/todos.sql
@@ -0,0 +1,16 @@
+-- create table task(
+--     id serial,
+--     name character varying (50),
+--     isdone character(3),
+--     PRIMARY KEY (id)
+-- );
+
+-- truncate task;
+
+-- insert into task (id, name, isdone) values (1,'Task A', 'NO');
+-- insert into task (name, isdone) values ('Task B', 'NO');
+-- insert into task (name, isdone) values ('Task C', 'NO');
+
+SELECT * FROM "public".task LIMIT 100;
+
+
